b0d29cdcfe7ff0e0c260278883cb471ca9c1cd6d
asmaaabozied <49614555+asmaaabozied@users.noreply.github.com>
2025-10-02 11:56:04 +0300

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+   .postCss('resources/css/app.css', 'public/css', [
+       require('tailwindcss'),
+       require('autoprefixer'),
+   ]);
+
+mix.js('resources/js/patient/show.js', 'public/js/patient/show.js');
+
+mix.styles(['resources/css/patient/show.css'], 'public/css/patient/show.css');
